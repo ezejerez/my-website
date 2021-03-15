2b560a3cca6cf0e32f75b06189e0ffd8cabec829
Leonardo Massaroli <leonardo.massaroli@globant.com>
2021-03-15 20:19:18 -0300

    fix icons (leo)

--- src/components/SocialFollow/SocialFollowElements.js
+++ src/components/SocialFollow/SocialFollowElements.js
@@ -1,15 +1,29 @@
 import styled from 'styled-components';
 
-export const SocialFollowIcon = styled.div`
-  size: 100px;
-  cursor: pointer;
-  color: red;
-`;
-
 export const SocialFollowIconsContainer = styled.div`
-  background: blue;
+  background: gray;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+
+  /* Contenedor de cada icon */
+  > a {
+    cursor: pointer;
+    color: red;
+  }
+
+  /* Cuando pasas el mouse encima del contenedor */
+  > a:hover {
+    color: blue;
+  }
+
+  /* 
+    Icon (metemos el width y height aca porque los contenedores si
+    no le seteas el tamaño, se adaptan al tamaño del hijo)
+  */
+  > a > svg {
+    width: 50px;
+    height: 50px;
+  }
 `;
--- src/pages/PagesElements.js
+++ src/pages/PagesElements.js
@@ -4,7 +4,5 @@ export const SocialFollowContainer = styled.div`
   position: fixed;
   left: 10px;
   bottom: 0px;
-  height: 1000px;
-  widht: 1000px;
   z-index: 9999;
 `;
