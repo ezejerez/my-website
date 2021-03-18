10cae425858c982cbc0a97da53edebc658286573
Braidlockk <ezejerez99@gmail.com>
2021-03-18 18:06:20 -0300

    modal-added

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 import {
   ContactSectionBackground,
@@ -10,42 +10,79 @@ import {
   StyledButton,
 } from './ContactElements';
 
-const ContactSection = () => {
-  function sendEmail(e) {
-    e.preventDefault();
+import Modal from '../Modal/index';
 
-    emailjs
-      .sendForm(
-        'gmail',
-        'template_wotpibf',
-        e.target,
-        'user_8o9zQhhFpdSCF4aIT7OSF'
-      )
-      .then(() => {
-        alert(
-          'Your message was sent successfully! I will contact you as soon as possible &#x1F60E;'
-        );
-      }, e.target.reset());
-  }
+const ContactSection = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal((prev) => !prev);
+  };
+
+  // function sendEmail(e) {
+  //   e.preventDefault();
+
+  //   emailjs
+  //     .sendForm(
+  //       'gmail',
+  //       'template_wotpibf',
+  //       e.target,
+  //       'user_8o9zQhhFpdSCF4aIT7OSF',
+  //     )
+  //     .then(() => {
+  //       alert(
+  //         'Your message was sent successfully! I will contact you as soon as possible &#x1F60E;',
+  //       );
+  //     }, e.target.reset());
+  // }
 
   return (
     <div id='contact'>
+      <Modal showModal={showModal} setShowModal={setShowModal} />
       <ContactSectionBackground>
         <StyledFormWrapper>
           <StyledForm>
             <h2>Contact me:</h2>
-            <label htmlFor='name'>Name</label>
-            <StyledInput type='text' name='name' />
-            <label htmlFor='email'>Your Email</label>
-            <StyledInput type='email' name='email' />
-            <label htmlFor='subject'>Subject</label>
-            <StyledInput type='subject' name='subject' />
+            <label htmlFor='name'></label>
+            <StyledInput
+              type='text'
+              className='form-control'
+              placeholder='Your name'
+              name='name'
+            />
+            <label htmlFor='email'></label>
+            <StyledInput
+              type='email'
+              className='form-control'
+              placeholder='Your email address'
+              name='email'
+            />
+            <label htmlFor='subject'></label>
+            <StyledInput
+              type='text'
+              className='form-control'
+              placeholder='Subject'
+              name='subject'
+            />
             <label htmlFor='message'></label>
-            <StyledTextArea name='subject' />
+            <StyledTextArea
+              className='form-control'
+              id=''
+              cols='30'
+              rows='8'
+              placeholder='Your message'
+              name='message'
+            />
             <StyledError>
               <p>Error message here</p>
             </StyledError>
-            <StyledButton type='submit'>Send Message</StyledButton>
+            <StyledButton
+              onClick={openModal}
+              type='submit'
+              className='btn btn-info'
+              value='Send Message'>
+              Send Message
+            </StyledButton>
           </StyledForm>
         </StyledFormWrapper>
       </ContactSectionBackground>
@@ -55,58 +92,4 @@ const ContactSection = () => {
 
 export default ContactSection;
 
-// ContactSectionContainer,
-// ContactSectionTitle,
-// ContactButton,
-
-{
-  /* <ContactSectionTitle>Contact me:</ContactSectionTitle>
-      <form onSubmit={sendEmail}>
-        <ContactSectionContainer>
-          <div className='col-8 form-group mx auto'>
-            <input
-              type='text'
-              className='form-control'
-              placeholder='Name'
-              name='name'
-            />
-          </div>
-
-          <div className='col-8 form-group pt-2 mx auto'>
-            <input
-              type='email'
-              className='form-control'
-              placeholder='Email address'
-              name='email'
-            />
-          </div>
-
-          <div className='col-8 form-group pt-2 mx auto'>
-            <input
-              type='text'
-              className='form-control'
-              placeholder='Subject'
-              name='subject'
-            />
-          </div>
-
-          <div className='col-8 form-group pt-2 mx auto'>
-            <textarea
-              className='form-control'
-              id=''
-              cols='30'
-              rows='8'
-              placeholder='Your message'
-              name='message'></textarea>
-          </div>
-
-          <div>
-            <ContactButton
-              type='submit'
-              className='btn btn-info'
-              value='Send Message'
-            />
-          </div>
-        </ContactSectionContainer>
-      </form> */
-}
+// agregar <StyledForm onSubmit={sendEmail}>
